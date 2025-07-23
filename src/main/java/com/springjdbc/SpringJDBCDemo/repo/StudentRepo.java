ffba5df0062419c039fb7956d6f5db2f1813d7ad
gaddganepola <ganeypola@gmail.com>
2025-07-23 21:25:12 +0530

    implement jdbc to make the connection with database

--- src/main/java/com/springjdbc/SpringJDBCDemo/repo/StudentRepo.java
+++ src/main/java/com/springjdbc/SpringJDBCDemo/repo/StudentRepo.java
@@ -1,6 +1,8 @@
 package com.springjdbc.SpringJDBCDemo.repo;
 
 import com.springjdbc.SpringJDBCDemo.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -8,12 +10,27 @@ import java.util.List;
 
 @Repository
 public class StudentRepo {
+
+    private JdbcTemplate jdbc;
+
     public void save(Student s) {
-        System.out.println("student saved");
+
+        String sql = "INSERT INTO student (rollno, name, marks) VALUES (?, ?, ?)";
+
+        jdbc.update(sql, s.getRollno(), s.getName(), s.getMarks());
     }
 
     public List<Student> findAll() {
         List<Student> students = new ArrayList<>();
         return students;
+    }
+
+    public JdbcTemplate getJdbc() {
+        return jdbc;
+    }
+
+    @Autowired
+    public void setJdbc(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
     }
 }
